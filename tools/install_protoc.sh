034cd69c5aee546ffefa6240290bbe68539f1b47
Evan <evanljp@outlook.com>
2020-12-28 14:32:21 +0800

    add protoc install

--- tools/install_protoc.sh
+++ tools/install_protoc.sh
@@ -26,5 +26,7 @@ fi
 PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+sudo chmod 755  /usr/local/bin/protoc
 rm -f $PROTOC_ZIP
+
+echo `protoc --version`
