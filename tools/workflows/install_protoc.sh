b73471d8507a89689a38b2ea7f5e65382a2ff787
Evan <evanljp@outlook.com>
2020-12-28 14:09:22 +0800

    add permissions

--- tools/workflows/install_protoc.sh
+++ tools/workflows/install_protoc.sh
@@ -17,6 +17,7 @@
 # limitations under the License.
 #
 
+
 PROTOC_ZIP=protoc-3.14.0.-linux-x86_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
